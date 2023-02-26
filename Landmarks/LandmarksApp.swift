8280b90cdf7db1ef9bc60a5e2d1594fb36af2a04
Danyu Rajbahak <rajbahakdanyu@gmail.com>
2023-02-26 19:18:33 +1000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by danyu rajbahak on 26/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
